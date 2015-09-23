f48087caa29bdc81c6dba1d1123e1cff9a8196fa
Taehun Kim <npngseja@npngsejas-macbook-pro.local>
2015-09-23 14:52:05 +0200

    Initial Commit

---
+++ MiniDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniDo
+//
+//  Created by npngseja on 23/09/15.
+//  Copyright © 2015 Taehun Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
